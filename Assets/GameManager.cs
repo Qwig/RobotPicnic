538803144050c041a5638d6b58069a2bb33eee5b
Qwig <Guido.vonMulert@gmail.com>
2017-09-20 20:15:52 +1200

    Initial Commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private MapGenerator _MapGenerator;
+
+    void Start ()
+    {
+        _MapGenerator = GetComponent<MapGenerator>();
+
+        _MapGenerator.CreateMap();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MovingObject
+{
+
+    public void Move(float x, float y)
+    {
+        ActuallyMove(x, y);
+    }
+
+
+
+
+
+    // Use this for initialization
+    void Start ()
+    {
+        base.Start();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
