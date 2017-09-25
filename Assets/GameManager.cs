2e5944c56636e1eb6bba66d6dfddd4b3a2c816aa
Qwig <Guido.vonMulert@gmail.com>
2017-09-25 21:24:40 +1300

    secret changes
    I'm looking at hami

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public int awesomeTurkeys;
+    public int awesomeTurkeys; //this is a test, you have passed
 
     private MapGenerator _MapGenerator;
 
